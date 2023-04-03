edb1e81791a8e70097e5f716a57709c6991d062c
Xin <129676368+endlessnoc@users.noreply.github.com>
2023-04-04 00:20:19 +0800

    Update LCM.py

--- Week2/LCM.py
+++ Week2/LCM.py
@@ -1,15 +1,12 @@
 #Programming Assignment 2: Algorithmic Warm-up, Least Common Multiple
-def leastcommonM(a, b):
-   if a > b:
-       big = a
-   else:
-       big = b
-   while(True):
-       if ((big%a == 0) and (big%b == 0)):
-           lcm = big
-       big += 1
-       break
-   return lcm
+def greatestCommonD(a, b):
+    if not b:
+        return a
+    return greatestCommonD(b, a % b)
 
-a,b = map(int, input().split())
-print(leastcommonM(a, b))
+def leastcommomM(a, b):
+    return (a * b) // greatestCommonD(a, b)
+
+if __name__ == "__main__":
+    a, b = map(int, input().split())
+    print(leastcommomM(a, b))
